9187b871bab8599974c302b6dbb6dc870daadfba
antonio00727 <jmarrero3483@gmail.com>
2022-06-17 23:45:47 -0400

    changed the game, and how it looks, making it working thru the weekend

--- index.js
+++ index.js
@@ -1,73 +1,23 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const tiles =Array.from(document.querySelector('.tile'))
-    const playerDisplay=documet.querySelector('.display-palyer')
-    const resetButton=document.querySelector('#reset')
-    const announce =document.querySelector('.announce')
+const boxes=Array.from(document.getElementsByClassName('box'))
+console.log(boxes)
 
-    let board = ['','','','','','','','','']
-    let Player = 'X'
-    let gameActive=true
-
-    const PlayerX_Won='PLAYERX_WON'
-    const Computer_Won='COMPUTER_WON'
-    const tie="Tie"
-
-    const winCondition=[
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
-
-    function handleResult(){
-        let won=false
-        for (let i=0;i<=7;i++){
-            
+const drawBoard=()=>{
+    boxes.forEach((box,index) => {
+        let styleStr = ''
+        if (index < 3){
+            styleStr += `border-bottom: 2px solid var(--navy);`
         }
-    }
-
-
-
-
-
-    const Announce=(type)=>{
-        switch(type){
-            case PlayerX_Won:
-                announce.innerHTML="Player<span class='playerX'></span> Won"
-                break
-            case Computer_Won:
-                announce.innerHTML="Computer<span class='computer'></span> Won"
-                break
-            case tie:
-                announce.innerHTML='Tie'
+        if(index % 3 == 0){
+            styleStr += `border-right: 2px solid var(--navy);`
         }
-        announce.classList.remove('hide')
-    }
-
-    const changePlayer=()=>{
-        playerDisplay.classList.remove('player${currentPlayer')
-        currentPlayer=currentPlayer==='X'?'O':'X'
-        playerDisplay.innerText=currentPlayer
-        playerDisplay.classList.add('player${currentPlayer}')
-    }
-
-    const playerAction=(tile,index)=>{
-        if(isValidation(tile)&& gameActive){
-            tile.innerText = currentPlayer
-            tile.classList.add('player${currentPlayer}')
-            updateBoard(index)
-            handleResult()
-            changePlayer()
+        if(index % 3 == 2){
+            styleStr += `border-left: 2px solid var(--navy);`
         }
-    }
+        if(index > 5){
+            styleStr += `border-top: 2px solid var(--navy);`
+        }
+        box.style=styleStr
+    });
+}
 
-    tiles.forEach( (tile,index)=> {
-        tile.addEventListener('click',()=> playerAction(tile,index()))
-    })
-
-    resetButton.addEventListener('click', reset)
-});
+drawBoard()
